098dd869510460320f929a3a783e53ff65d06171
404-5971 <141177946+404-5971@users.noreply.github.com>
2026-01-05 17:17:11 -0600

    refactor: configure diagnostic signs directly within `vim.diagnostic.config`

--- common/.config/nvim/lua/plugins/lspconfig.lua
+++ common/.config/nvim/lua/plugins/lspconfig.lua
@@ -12,18 +12,18 @@ return {
 
     vim.diagnostic.config({
       virtual_text = true, -- Show text next to the error
-      signs = true, -- Show icons in the sidebar
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.HINT] = "󰠠 ",
+          [vim.diagnostic.severity.INFO] = " ",
+        },
+      },
       underline = true, -- Underline the error
       update_in_insert = false, -- Don't yell at me while I'm still typing
       severity_sort = true, -- Show errors before warnings
     })
-
-    -- 2. Define Custom Icons (requires a Nerd Font)
-    local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-    for type, icon in pairs(signs) do
-      local hl = "DiagnosticSign" .. type
-      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-    end
 
     mason_lspconfig.setup({
       -- This 'handlers' table replaces the old 'setup_handlers' function
